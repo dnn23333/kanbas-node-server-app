ae8239f6dbf20d4a665aabdbb75e66eb42116b03
董寧寧 <dongningning@dongningningdeMacBook-Air.local>
2023-12-16 10:40:33 -0800

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import Lab5 from "../kanbas-react-web-app/src/lab5.js";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+Hello(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
--- courses/routes.js
+++ courses/routes.js
@@ -1,4 +1,4 @@
-import Database from "../Database";
+import Database from "../Database/index.js";
 function CourseRoutes(app) {
 
   app.get("/api/courses/:id", (req, res) => {
